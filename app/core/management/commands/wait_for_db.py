ad886af328167e8c7d228207debffa2de2111910
dr-zieba <zieba1jan@gmail.com>
2023-11-22 21:14:22 +0100

    Unit test for wait_for_db command

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,12 @@
+'''
+Command to wait for the database before django will start
+'''
+
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    '''
+    Wait for database command
+    '''
+    def handle(self, *args, **options):
+        pass
---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,28 @@
+'''
+Test commands
+'''
+from unittest.mock import patch
+from psycopg2 import OperationalError as Psycopq2Error
+
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import SimpleTestCase
+
+@patch('core.management.commands.wait_for_db.Command.check')
+class CommandTest(SimpleTestCase):
+    ''' Test class for commands '''
+    def test_wait_for_db_ready(self, patched_check):
+        ''' Test when db is ready '''
+        patched_check.return_value = True
+        call_command('wait_for_db')
+        patched_check.assert_called_once_with(database=['default'])
+
+    @patch('time.sleep')
+    def test_wait_for_db_not_ready(self, patched_sleep, patched_check):
+        ''' Test when db is not ready '''
+
+        # Simulates checks with returned values like Psycopq2Error, OperationalError and True = db finally got ready
+        patched_check.side_effects = [Psycopq2Error] * 2 + [OperationalError] * 3 + [True]
+        self.assertEquals(patched_check.call_count, 6)
+        patched_check.assert_called_with(database=['default'])
+
