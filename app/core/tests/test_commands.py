3ddd74fddce2df94bf135b010be9149bd5d45d24
dr-zieba <zieba1jan@gmail.com>
2024-10-01 21:19:01 +0200

    new commit

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,6 +1,7 @@
-'''
+"""
 Test commands
-'''
+"""
+
 from unittest.mock import patch
 from psycopg2 import OperationalError as Psycopq2Error
 
@@ -8,21 +9,24 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
-@patch('core.management.commands.wait_for_db.Command.check')
-class CommandTest(SimpleTestCase):
-    ''' Test class for commands '''
-    def test_wait_for_db_ready(self, patched_check):
-        ''' Test when db is ready '''
-        patched_check.return_value = True
-        call_command('wait_for_db')
-        patched_check.assert_called_once_with(database=['default'])
 
-    @patch('time.sleep')
+@patch("core.management.commands.wait_for_db.Command.check")
+class CommandTest(SimpleTestCase):
+    """Test class for commands"""
+
+    def test_wait_for_db_ready(self, patched_check):
+        """Test when db is ready"""
+        patched_check.return_value = True
+        call_command("wait_for_db")
+        patched_check.assert_called_once_with(database=["default"])
+
+    @patch("time.sleep")
     def test_wait_for_db_not_ready(self, patched_sleep, patched_check):
-        ''' Test when db is not ready '''
+        """Test when db is not ready"""
 
         # Simulates checks with returned values like Psycopq2Error, OperationalError and True = db finally got ready
-        patched_check.side_effects = [Psycopq2Error] * 2 + [OperationalError] * 3 + [True]
+        patched_check.side_effects = (
+            [Psycopq2Error] * 2 + [OperationalError] * 3 + [True]
+        )
         self.assertEquals(patched_check.call_count, 6)
-        patched_check.assert_called_with(database=['default'])
-
+        patched_check.assert_called_with(database=["default"])
